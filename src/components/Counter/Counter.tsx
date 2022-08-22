325cc2de6f2ba29c1ba5429946e6e4b5b2b0bc6f
Xifre Font <xifre.font.97@gmail.com>
2022-08-22 16:28:31 +0200

    Set global date and set an initial state for the counter component

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,24 +1,36 @@
+import { useState } from "react";
+import keyDate from "../../data/keyDate";
 import CounterStyled from "./CounterStyled";
 
-const Counter = (): JSX.Element => (
-  <CounterStyled>
-    <li className="counter-item">
-      <span className="counter-item__number">02</span>
-      <span className="counter-item__data">days</span>
-    </li>
-    <li className="counter-item">
-      <span className="counter-item__number">20</span>
-      <span className="counter-item__data">hours</span>
-    </li>
-    <li className="counter-item">
-      <span className="counter-item__number">34</span>
-      <span className="counter-item__data">minutes</span>
-    </li>
-    <li className="counter-item">
-      <span className="counter-item__number">09</span>
-      <span className="counter-item__data">seconds</span>
-    </li>
-  </CounterStyled>
-);
+const initialState = {
+  day: keyDate.getDate(),
+  hour: keyDate.getHours(),
+  minute: keyDate.getMinutes(),
+  second: keyDate.getSeconds(),
+};
 
+const Counter = (): JSX.Element => {
+  const [counter, setCounter] = useState(initialState);
+
+  return (
+    <CounterStyled>
+      <li className="counter-item">
+        <span className="counter-item__number">{counter.day}</span>
+        <span className="counter-item__data">days</span>
+      </li>
+      <li className="counter-item">
+        <span className="counter-item__number">{counter.hour}</span>
+        <span className="counter-item__data">hours</span>
+      </li>
+      <li className="counter-item">
+        <span className="counter-item__number">{counter.minute}</span>
+        <span className="counter-item__data">minutes</span>
+      </li>
+      <li className="counter-item">
+        <span className="counter-item__number">{counter.second}</span>
+        <span className="counter-item__data">seconds</span>
+      </li>
+    </CounterStyled>
+  );
+};
 export default Counter;
---
+++ src/data/keyDate.ts
@@ -0,0 +1,3 @@
+const keyDate = new Date(0, 0, 2, 20, 34, 9);
+
+export default keyDate;
